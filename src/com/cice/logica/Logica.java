368a126bb64927b885c59a529c76b052a2896ed9
diegodf8 <diegodf8@gmail.com>
2017-12-10 12:10:53 +0100

    Subida Diego

--- src/com/cice/logica/Logica.java
+++ src/com/cice/logica/Logica.java
@@ -1,4 +1,4 @@
-/* Contiene todos los métodos logicos la aplicacion
+/* Contiene todos los métodos logicos la aplicación
 
 */
 package com.cice.logica;
@@ -9,17 +9,39 @@ public class Logica {
     public int sumar(int numA, int numB){
         int resultado = 0;
 
-        
+        resultado = numA + numB;
 
         return resultado;
     }
 
 
     //Resta
+    public int restar(int numA, int numB){
+        int resultado = 0;
+
+        resultado =  numA - numB;
+
+        return resultado;
+    }
 
     //Multiplicacion
+    public int multiplicar (int numA, int numB){
+        int resultado=0;
+
+        resultado = numA * numB;
+
+        return resultado;
+    }
 
     //Division
+    public int dividir (int numA, numA){
+        int resultado = 0;
+
+        resultado = numA / numB;
+
+        return  resultado;
+
+    }
 
 
 }
