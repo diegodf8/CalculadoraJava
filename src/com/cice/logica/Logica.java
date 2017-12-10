9c01cb1f405e11ce5ff67d5c2d3fc12261f8a581
diegodf8 <diegodf8@gmail.com>
2017-12-10 12:06:28 +0100

    Initial commit

---
+++ src/com/cice/logica/Logica.java
@@ -0,0 +1,25 @@
+/* Contiene todos los métodos logicos la aplicacion
+
+*/
+package com.cice.logica;
+
+public class Logica {
+
+    //Suma
+    public int sumar(int numA, int numB){
+        int resultado = 0;
+
+        
+
+        return resultado;
+    }
+
+
+    //Resta
+
+    //Multiplicacion
+
+    //Division
+
+
+}
